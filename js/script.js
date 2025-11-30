a09515ad08139640ca1394804027b79f9f7846b3
AmirMJ <amirjvan0@gmail.com>
2025-11-30 16:39:47 +0330

    complet bottom nav working

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let pages = document.querySelectorAll(".page")
+let nav_items = document.querySelectorAll("#bottom_nav a")
+let home_page = document.querySelector("#home_page")
+let path_icons = document.querySelectorAll("#bottom_nav path")
+
+
+function displayNoneAll(){
+    pages.forEach(page => {
+        page.classList.add("display-none")
+        page.classList.remove("display-block")
+    });
+}
+function fill_icon(){
+    path_icons.forEach(path_icon => {
+        path_icon.classList.add("icon_fill")
+    });
+}
+function remove_after_line(){
+    nav_items.forEach(nav_item => {
+        nav_item.classList.remove("after-line")
+    });
+}
+
+fill_icon()
+
+displayNoneAll()
+home_page.classList.add("display-block")
+
+let home_icon_path = document.getElementById("home_icon_path")
+home_icon_path.classList.remove("icon_fill")
+
+
+nav_items.forEach(nav_item => {
+    nav_item.addEventListener("click",()=>{
+        displayNoneAll()
+        fill_icon()
+        remove_after_line()
+
+        nav_item.classList.add("after-line")
+
+        let nav_id = nav_item.getAttribute("id")
+        let paths = document.querySelectorAll(`#${nav_id} path`)
+        paths.forEach(path => {
+            path.classList.remove("icon_fill")
+        });
+
+        let profile_page = nav_item.getAttribute("pageSelector")
+        let page_selected = document.getElementById(profile_page)
+        page_selected.classList.add("display-block")
+        page_selected.classList.remove("display-none")
+        
+    })
+});

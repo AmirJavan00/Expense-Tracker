75ecd4a7ce1d1e55e4c4422d93603431b142ae2b
AmirMJ <amirjvan0@gmail.com>
2025-12-26 00:56:47 +0330

    create Class Category for add category

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
-// ----class----
+// ------------class------------
+class Category {
+    categories_arr = JSON.parse(localStorage.getItem("categories")) || [];
+    category_list_html = document.querySelector(".category_list")
 
-// ----variables----
+    
+    constructor(name, icon) {
+        this.cat_name = name;
+        this.cat_icon = icon;
+        this.cat_color = randomColor(); 
+        this.create(this.cat_name, this.cat_icon, this.cat_color);
+    }
 
+    create(name, icon, color) {
+        this.category_obj = { "name": name, "icon": icon, "color": color };
+        this.update(this.category_obj);
+    }
+    
+    update(object) {
+        this.categories_arr.push(object);
+        localStorage.setItem("categories", JSON.stringify(this.categories_arr));
+        this.loadHtml(object)
+    }
+
+    loadHtml(category){
+            this.category_list_html.insertAdjacentHTML("beforeend",`
+                <div class="category_item">
+                    <div class="category_content">
+                        <div class="line" style="background-color:${category.color};"></div>
+                        <div class="category_logo">${category.icon}</div>
+                        <div class="category_title">${category.name}</div>
+                    </div>
+                    <div class="delete_category">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64"><defs><style>.cls-1{fill:url(#linear-gradient);}.cls-2{fill:url(#linear-gradient-2);}</style><linearGradient id="linear-gradient" x1="5.85" y1="32" x2="58.15" y2="32" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffae46"/><stop offset="0.19" stop-color="#ff9755"/><stop offset="0.74" stop-color="#ff5a7d"/><stop offset="1" stop-color="#ff428d"/></linearGradient><linearGradient id="linear-gradient-2" x1="22.35" y1="32" x2="41.65" y2="32" xlink:href="#linear-gradient"/></defs><g id="Layer_7" data-name="Layer 7"><path class="cls-1" d="M50.5,13.5a26.15,26.15,0,1,0,0,37A26.18,26.18,0,0,0,50.5,13.5ZM47.67,47.67a22.16,22.16,0,1,1,0-31.34A22.17,22.17,0,0,1,47.67,47.67Z"/><path class="cls-2" d="M41.06,22.94a2,2,0,0,0-2.83,0L32,29.17l-6.23-6.23a2,2,0,0,0-2.83,2.83L29.17,32l-6.23,6.23a2,2,0,1,0,2.83,2.83L32,34.83l6.23,6.23a2,2,0,0,0,2.83-2.83L34.83,32l6.23-6.23A2,2,0,0,0,41.06,22.94Z"/></g></svg>
+                    </div>
+                </div>
+                `)
+    }
+}
+
+
+// ------------variables------------
 // مقدار های قسمت نویگیشن پایین
 let pages = document.querySelectorAll(".page")
 let nav_items = document.querySelectorAll("#bottom_nav a")
@@ -17,8 +55,32 @@ let btn_close_add_transaction = document.querySelector("#btn_close_add_transacti
 let btn_add_category = document.querySelector(".btn_add_category")
 let btn_close_add_category = document.querySelector("#btn_close_add_category")
 let add_category_section = document.querySelector("#add_category_section")
+let add_category_submit = document.querySelector("#add_category_submit")
+let inp_category_name
+let inp_category_icon
+let category_object
+// ------------eventlisteners------------
 
-// ----eventlisteners----
+add_category_submit.addEventListener("click",()=>{
+    inp_category_name = document.querySelector("#inp_category_name")
+    inp_category_icon = document.querySelector(".icons_option input:checked + label")
+    category_object = new Category(inp_category_name.value, inp_category_icon.innerHTML)
+    add_category_section.classList.add("display-none")
+})
+
+function randomColor(){
+    /**
+     * تولید یک کد رنگی به صورت تصادفی و برگشت دادن آن به صورت رشته 
+     */
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
 
 // بخش اضافه کردن دسته بندی
 btn_add_category.addEventListener("click",()=>{
